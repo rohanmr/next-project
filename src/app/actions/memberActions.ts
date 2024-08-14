be1caf91b7985b08cc2eb28299aba5ed2c0237e6
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-08-14 23:41:17 +0530

    added member sidebar and members detailed page

--- src/app/actions/memberActions.ts
+++ src/app/actions/memberActions.ts
@@ -18,3 +18,16 @@ export async function getMembers(){
         console.log(error)
     }
 }
+
+export async function getMemberByUserId(userId:string){
+    try {
+        return prisma.member.findUnique({
+            where:{
+                userId
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+}
