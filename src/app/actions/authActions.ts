74260d7d0ac77ca1506992e4d574a73315918528
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-10-04 22:23:01 +0530

    added some code for user like feature

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -1,5 +1,5 @@
 "use server";
-import { signIn, signOut } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
 import { prisma } from "@/lib/prisma";
 import { LoginSchema } from "@/lib/schemas/loginSchema";
 import { registerSchema, RegisterSchema } from "@/lib/schemas/registerSchema";
@@ -75,4 +75,13 @@ export async function getUserByEmail(email:string){
 
 export async function getUserById(id:string){
     return prisma.user.findUnique({where:{id}})
+}
+
+export async function getAuthUserId(){
+    const session = await auth()
+    const userId = session?.user?.id
+
+    if (!userId) throw new Error("Unauthorized")
+        return userId
+
 }
