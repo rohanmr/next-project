7941c391a4000a808ab7c23758d0914c8ea3d681
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-10-05 23:37:31 +0530

    created like button

--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -1,3 +1,4 @@
+import LikeButton from "@/components/LikeButton";
 import { calculateAge } from "@/lib/util";
 import { Card, CardFooter, Image } from "@nextui-org/react";
 import { Member } from "@prisma/client";
@@ -17,6 +18,9 @@ export default function MemberCard({ member }: Props) {
         src={member.image || "/images.user.png"}
         className="aspect-square object-cover"
       />
+      <div className="absolute top-3 right-3 z-30">
+        <LikeButton targetId={member.userId} hasLiked={true} />
+      </div>
       <CardFooter className="flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient">
         <div className="flex flex-col text-white">
           <span className="font-semibold">
